901a27e07cd3d2cd299128c0db84619d00d0d6c5
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2021-01-21 15:39:43 +0700

    New cleanup unusage code 😪

---
+++ components/header/styles.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+const container = css`
+  min-height: 100vh;
+  padding: 1 0.5rem;
+  display: grid;
+  place-items: center;
+`
+
+const title = css`
+  font-size: 4rem;
+  text-align: center;
+`
+
+export { container, title }
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '../components/header'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>All about NextJS ⚡</title>
+      </Head>
+
+      <main>
+        <Header />
+      </main>
+    </>
+  )
+}
